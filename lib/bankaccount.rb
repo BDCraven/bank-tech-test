e9a516b6df04469756dab763802a38a33ee33cf7
BDCraven <bencraven@hotmail.com>
2017-09-05 12:50:40 +0100

    Bank account has an empty statement when initialized

---
+++ lib/bankaccount.rb
@@ -0,0 +1,11 @@
+class BankAccount
+
+  def statement
+    statement_header
+  end
+
+  def statement_header
+    "date || credit || debit || balance"
+  end
+
+end
--- spec/bankaccount_spec.rb
+++ spec/bankaccount_spec.rb
@@ -1,2 +1,10 @@
+require 'bankaccount'
+
 describe BankAccount do
+  subject(:bankaccount) { described_class.new }
+
+  it 'has an empty statement when initialized' do
+    statement = 'date || credit || debit || balance'
+    expect(bankaccount.statement).to eq(statement)
+  end
 end
